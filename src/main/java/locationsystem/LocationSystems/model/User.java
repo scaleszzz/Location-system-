d579eb373271ebf08e6f9ee9130ed4cc2c7e4783
Aibar <aibar03aibar@gmail.com>
2024-08-07 10:54:14 +0500

    created Model User

--- src/main/java/locationsystem/LocationSystems/model/User.java
+++ src/main/java/locationsystem/LocationSystems/model/User.java
@@ -1,2 +1,35 @@
-package locationsystem.LocationSystems.model;public class User {
+package locationsystem.LocationSystems.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "t_user")
+public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull
+    @Column(name = "name")
+    private String name;
+
+    @NotNull
+    @Email
+    @Column(name = "email")
+    private String email;
+
+    @NotNull
+    @Column(name = "password")
+    private String password;
+
 }
